b48c8b1bfbe1138bc7a1f58c1774e38fd4c025c1
mo1257 <sknowwhite13@gmail.com>
2024-07-04 22:20:46 +0900

    バリデーション機能

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
-end
+    validates :title, presence: true, length: {maximum: 20 }
+    validates :memo, length: { maximum: 500 }
+    validates :start_date, presence: true
+    validates :end_date, presence:true
+    validate :start_end_check
+   
+    def start_end_check
+     return if start_date.blank? || end_date.blank?
+     errors.add(:end_date, "は開始日以降の日付で選択してください") if self.start_date > self.end_date
+   end
+   end
